aa70dab2cf50926195728c4dc9253b366828a22f
Somnath Sopan Shirsath <83588073+sss300800@users.noreply.github.com>
2021-09-07 12:25:07 +0530

    Add files via upload

---
+++ assignment8/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment8
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            var mytuple = Tuple.Create(2, "somnath", 9404300800, true, 0.23, 123, false, "veer");
+            Console.WriteLine(mytuple.Item1);
+            Console.WriteLine(mytuple.Item2);
+            Console.WriteLine(mytuple.Item3);
+            Console.WriteLine(mytuple.Item4);
+
+            Console.WriteLine(mytuple.Item5);
+            Console.WriteLine(mytuple.Item6);
+            Console.WriteLine(mytuple.Item7);
+            Console.WriteLine(mytuple.Rest);
+            Console.ReadLine();
+
+            '       }
+                
+    }
+}
